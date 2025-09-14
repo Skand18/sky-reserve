dea6850591a7504980386e44f211433d5f55ba69
Skand Kumar <skand@Skands-MacBook-Air.local>
2025-09-14 17:04:20 +0530

    Added new migration that can add constraint in a column of Airport table
    ```
    npx sequelize migration:generate --name update-city-airport-association
    ```

---
+++ src/migrations/20250914105150-update-city-ariport-association.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Airport', {
+      fields: ['cityId'],
+      type: 'FOREIGN KEY',
+      name: 'city_fk_constraint',
+      references:{
+        table:'City',
+        field:'id'
+      },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE'
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+   await queryInterface.removeConstraint('Airport','city_fk_constraint')
+  }
+};
