959a9868a0c2916a19198955b73dd427c442e460
Skand Kumar <skand@Skands-MacBook-Air.local>
2025-09-28 20:19:46 +0530

    added filters for get all flights

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -6,16 +6,8 @@ const router = express.Router();
 // /api/v1/Flights POST 
 router.post('/', FlightMiddlewares.validateCreateRequest, FlightController.createFlight)
 
-// // /api/v1/Flights GET
-// router.get('/', FlightController.getFlights)
+// /api/v1/Flights GET 
+router.get('/', FlightController.getAllFlights)
 
-// // api/v1/Flights:id GET
-// router.get('/:id', FlightController.getFlight)
-
-// // api/v1/Flights:id DELETE
-// router.delete('/:id', FlightController.deleteFlight)
-
-// api/v1/ariports/:id UPDATE
-// router.patch('/:id' , FlightController.updateFlight)
 
 module.exports = router;
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const {FlightRepository } = require('../repositories');
 const { dateTimeHelper } = require('../utils/')
+const { Op } = require( 'sequelize');
 const AppError = require('../utils/errors/app-error');
 
  const flightRepository = new FlightRepository();
@@ -30,6 +31,47 @@ const AppError = require('../utils/errors/app-error');
     }
  }
 
+ async function getAllFlights(query) {
+    try {
+        let customFilter = {};
+        let sortFilter = [];
+        if(query.trips){
+            [depAirportId, arrAirportId] = query.trips.split('-');
+            customFilter.depAirportId = depAirportId;
+            customFilter.arrAirportId = arrAirportId;
+        }
+        if(query.price){
+            [minPrice , maxPrice] = query.price.split('-');
+            customFilter.price = {
+                [Op.between] : [minPrice, ((maxPrice == undefined)?20000 : maxPrice)]
+            }
+        }
+        if(query.travellers){
+            customFilter.totalSeats = {
+                [Op.gte]: query.travellers
+            }
+        }
+        if(query.tripDate){
+            const startOfDay = new Date(`${query.tripDate}T00:00:00.000Z`);
+            const endOfDay   = new Date(`${query.tripDate}T23:59:59.999Z`);
+            
+            customFilter.depTime = {
+                [Op.between]: [startOfDay, endOfDay]
+            };
+        }
+        if(query.sort){
+            const params = query.sort.split(',');
+            const sortFilters = params.map((param)=> param.split('_'));
+            sortFilter = sortFilters;
+        }
+        const flights = await flightRepository.getAllFlights(customFilter,sortFilter);
+        return flights;
+    } catch (error) {
+        throw new AppError('Cannot fetch data of all the flights', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+ }
+
  module.exports = {
     createFlight,
+    getAllFlights
  }
